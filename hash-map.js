cbb81828fa6526b09965b21348b95663fe456706
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-18 05:39:49 +0200

    Create HashMap class with hash() and set()

---
+++ hash-map.js
@@ -0,0 +1,79 @@
+import { Node, LinkedList } from "./linked-list.js";
+
+class HashMap {
+    constructor() {
+        this.capacity = 16;
+        this.loadFactor = 0.75;
+        this.size = 0;
+        this.buckets = new Array(16).fill(undefined);
+    }
+
+    hash(key) {
+        if (typeof key !== "string") {
+            throw new TypeError("Keys must be strings");
+        }
+
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode = hashCode % this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+        const hash = this.hash(key);
+
+        if (this.buckets[hash] === undefined) {
+            const node = new Node();
+            node.key = key;
+            node.value = value;
+
+            this.buckets[hash] = new LinkedList();
+            this.buckets[hash].head = node;
+            this.size++;
+            return;
+        }
+
+        let temp = this.buckets[hash].head;
+
+        while (temp !== null) {
+            if (temp.key === key) {
+                temp.value = value;
+                return;
+            }
+
+            if (temp.nextNode === null) {
+                const node = new Node();
+                node.key = key;
+                node.value = value;
+
+                temp.nextNode = node;
+                this.size++;
+                return;
+            }
+
+            temp = temp.nextNode;
+        }
+    }
+
+}
+
+/* Tests */
+
+const hash = new HashMap();
+hash.set("Rama", "the old value");
+// console.log(hash.buckets[3].head);
+
+hash.set("Rama", "the new value");
+// console.log(hash.buckets[3].head);
+
+hash.set("Sita", "something new");
+// console.log(hash.buckets[3].head);
+
+console.log(hash.has("Doe"));
+
+// console.log(hash.buckets);
