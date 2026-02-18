51a4cd9f610e20f2824de50da8411d5535a2274b
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-18 04:56:21 +0200

    remove LinkedList append() and add Node key prop

--- linked-list.js
+++ linked-list.js
@@ -1,43 +1,26 @@
-export { Node, LinkedList }
+export { Node, LinkedList };
 class Node {
-    constructor() {
-        this.value = null;
-        this.nextNode = null;
-    }
+  constructor() {
+    this.key = null;
+    this.value = null;
+    this.nextNode = null;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  size() {
+    let counter = 0;
+    let temp = this.head;
+
+    while (temp !== null) {
+      counter++;
+      temp = temp.nextNode;
     }
 
-    append(value) {
-        const node = new Node();
-        node.value = value;
-
-        if (this.head === null) {
-            this.head = node;
-            return;
-        }
-
-        let temp = this.head;
-
-        while (temp.nextNode !== null) {
-            temp = temp.nextNode;
-        }
-
-        temp.nextNode = node;
-    }
-
-    size() {
-        let counter = 0;
-        let temp = this.head;
-
-        while (temp !== null) {
-            counter++;
-            temp = temp.nextNode;
-        }
-
-        return counter;
-    }
+    return counter;
+  }
 }
