f32fdc5f92a960533c1c8f4f16975c174f673bc1
Mostafa Imam <137308525+mostafa-imam@users.noreply.github.com>
2026-02-18 08:17:32 +0200

    Add test to main.js

--- hash-map.js
+++ hash-map.js
@@ -1,4 +1,5 @@
 import { Node, LinkedList } from "./linked-list.js";
+export { HashMap };
 
 class HashMap {
   constructor() {
@@ -242,29 +243,3 @@ class HashMap {
     }
   }
 }
-
-/* Tests */
-
-const test = new HashMap();
-
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
-test.set("grape", "purple");
-test.set("hat", "black");
-test.set("ice cream", "white");
-test.set("jacket", "blue");
-test.set("kite", "pink");
-test.set("lion", "golden");
-console.log(test.capacity);
-console.log(test.buckets);
-
-console.log("----------------------------------");
-
-test.set("moon", "silver");
-
-console.log(test.capacity);
-console.log(test.buckets);
